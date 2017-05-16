c70475a30feba3be080cad960ed91821f2c24bd3
Zhyhunenko <serhii.zhyhunenko@degitallyinspired.com>
2017-05-16 16:25:40 +0300

    c1

--- src/main/java/com/example/aspects/AspectToLogAdditionInfo.java
+++ src/main/java/com/example/aspects/AspectToLogAdditionInfo.java
@@ -3,21 +3,23 @@ package com.example.aspects;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.aop.BeforeAdvice;
+
+import java.lang.reflect.Method;
 
 /**
  * Created by sza on 15.05.2017.
  */
 
 @org.aspectj.lang.annotation.Aspect
-public class AspectToLogAdditionInfo {
+public class AspectToLogAdditionInfo implements BeforeAdvice {
     private static final Logger LOGGER = Logger.getRootLogger();
 
     public void logInit(){
         LOGGER.setLevel(Level.INFO);
     }
 
-    public void beforeCheck(ProceedingJoinPoint jp){
-
+    public void beforeCheck(ProceedingJoinPoint jp) throws Throwable {
         Object[] args = jp.getArgs();
         String password = (String) args[0];
 
